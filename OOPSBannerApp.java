cbd0276194627a61b199a4244042aef3e04a8a0d
ds5692-wq <ds5692@srmist.edu.in>
2026-02-16 14:09:58 +0530

    [DHAIRYA SAHU] Added:UC1- Print OOPSBannner

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp serves as the entry point for the application.
+ * UC1: Printing the literal text "OOPS" to the console.
+ * * @author Your Name
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method acting as the application's entry point.
+     */
+    public static void main(String[] args) {
+        // Method Invocation via System.out.println()
+        System.out.println("OOPS");
+    }
+}
