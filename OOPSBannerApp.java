cb0bf1345671e91e007687557347d93d49ef4cf4
ds5692-wq <ds5692@srmist.edu.in>
2026-02-17 13:22:56 +0530

    UC2: ASCII banner OOPS implementation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,14 @@
-/**
- * OOPSBannerApp serves as the entry point for the application.
- * UC1: Printing the literal text "OOPS" to the console.
- * * @author Your Name
- * @version 1.0
- */
 public class OOPSBannerApp {
 
-    /**
-     * Main method acting as the application's entry point.
-     */
     public static void main(String[] args) {
-        // Method Invocation via System.out.println()
-        System.out.println("OOPS");
+
+        System.out.println(" *****    *****    *****    *****  ");
+        System.out.println("*     *  *     *  *     *  *     * ");
+        System.out.println("*     *  *     *  *     *  *       ");
+        System.out.println("*     *  *     *  *******  ******* ");
+        System.out.println("*     *  *     *  *              * ");
+        System.out.println("*     *  *     *  *        *     * ");
+        System.out.println(" *****    *****   *         *****  ");
+
     }
 }
